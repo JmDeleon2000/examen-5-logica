8cca93985945d1e07d61afe00720a0c16ecf76ad
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-11-09 16:15:28 -0600

    final

--- turingMachineInterpreter.py
+++ turingMachineInterpreter.py
@@ -2,44 +2,102 @@ import json
 
 
 machineDef = {}
-with open('turingMachine.tur', 'r') as machine:
+file = input('Input file for machine definition: ')
+with open(file, 'r') as machine:
     machineDef = json.loads(machine.read())
-
-print('Loaded machine: ')
-print('M = {')
-print(machineDef['Q'])
-print(machineDef['Sigma'])
-print(machineDef['Gamma'])
-print(machineDef['Delta'])
-print(machineDef['Q0'])
-print(machineDef['Qaccept'])
-print(machineDef['Qreject'])
-print('}')
 
 print('Input chain: ')
 chain = input()
-currState = machineDef['Q0']
-head = 0
 
-while True:
-    if currState == machineDef['Qaccept']:
-        print('Input chain accepted')
-        break
-    if currState == machineDef['Qreject']:
-        print('Input chain rejected')
-        break
+def verification(chain):
+    if len(machineDef['Q']) < 1:
+        print('No states were specified')
+        return False
+    if len(machineDef['Delta']) < 1:
+        print('No transitions were specified')
+        return False
+    if len(machineDef['Gamma']) < 1:
+        print('No tape alphabet was specified')
+        return False
+    if not(machineDef['Qaccept'] in machineDef['Q']):
+        print('Acceptation state is not specified in the state list')
+        return False
+    if not(machineDef['Qreject'] in machineDef['Q']):
+        print('Rejection state is not specified in the state list')
+        return False
+    if not(machineDef['Q0'] in machineDef['Q']):
+        print('Starting state is not specified in the state list')
+        return False
     
-    if head == len(chain):
-        chain+=' '
-
-    if head < 0:
-        head = 0
-
-    rule = machineDef['Delta'][currState][chain[head]]
-    print('current chain: ' + chain + ' state: ' + currState + ' read: ' + chain[head] + ' rule: ' + str(rule))
-    chain = chain[:head] + rule['write'] + chain[head+1:]
-    currState = rule['next']
-    head += rule['move']
+    for i in machineDef['Delta'].values():
+        for j in i.values():
+            if not(j['write'] in machineDef['Gamma']):
+                print('State machine can write characters that are not specified in the tape character list')
+                return False
     
-    
+    for i in chain:
+        if not(i in machineDef['Sigma']):
+            print('Input chain contains characters that are not specified in the valid character list')
+            return False
 
+
+    print('Loaded machine: ')
+    print('M = {')
+    print(machineDef['Q'])
+    print(machineDef['Sigma'])
+    print(machineDef['Gamma'])
+    print(machineDef['Delta'])
+    print(machineDef['Q0'])
+    print(machineDef['Qaccept'])
+    print(machineDef['Qreject'])
+    print('}')
+    return True
+
+output = ''
+if verification(chain):
+
+    currState = machineDef['Q0']
+    head = 0
+    running = True
+    iterations = 100
+    while running:
+        for i in range(iterations):
+            if currState == machineDef['Qaccept']:
+                output += 'Input chain accepted'
+                print('Input chain accepted')
+                running = False
+                break
+            if currState == machineDef['Qreject']:
+                output += 'Input chain rejected'
+                print('Input chain rejected')
+                running = False
+                break
+
+            if head == len(chain):
+                chain+=' '
+
+            if head < 0:
+                head = 0
+
+            rule = machineDef['Delta'][currState][chain[head]]
+            line = 'tape state: ' + chain + ' state: ' + currState 
+            line += ' read: ' + (chain[head] if chain[head] != ' ' else '_') + ' rule: ' 
+            line += ' next state: ' + rule['next']
+            line += ' write: ' + (rule['write'] if rule['write'] != ' ' else '_')
+            line += ' move: ' + ('Right' if rule['move'] == 1 else 'Left')
+            print(line)
+            line += '\n'
+            output += line
+            chain = chain[:head] + rule['write'] + chain[head+1:]
+            currState = rule['next']
+            head += rule['move']
+        if (running):
+            print('The cap of execution has been reached.')
+            iterations = int(input('If you wish to continue processing, input the amount of iterations to execute (or -1 to stop execution): \n'))
+            if iterations < 0:
+                output+= 'Execution stoped by user'
+                print('Execution stoped by user')
+                break
+
+with open('InterpreterLog.log', 'w') as log:
+    log.write(output)
--- turingToJSONFormater.py
+++ turingToJSONFormater.py
@@ -16,30 +16,67 @@ machineDef = {
 #la maquina pedira la cantidad de iteraciones que se quieren imprimir cada vez que imprime
 #la cinta será un string y siempre tendrá ' ' al final. Si se intenta accesar a -1 se hará:
 #string = ' ' + string y se cambiará el index a 0
-machineDef['Q'] = ['q1', 'q2', 'q3', 'q4', 'q5', 'qaccept', 'qreject']
-machineDef['Sigma'] = ['0']
-machineDef['Gamma'] = ['0', 'x', ' ']
+#machineDef['Q'] = ['q1', 'q2', 'q3', 'q4', 'q5', 'qaccept', 'qreject']
+#machineDef['Sigma'] = ['0']
+#machineDef['Gamma'] = ['0', 'x', ' ']
+#machineDef['Q0'] = 'q1'
+#machineDef['Qaccept'] = 'qaccept'
+#machineDef['Qreject'] = 'qreject'
+#machineDef['Delta'] = {
+#    'q1':{'0':{'next':'q2', 'write': ' ', 'move': 1}, 
+#            ' ':{'next':'qreject', 'write': ' ', 'move': 1}, 
+#            'x':{'next':'qreject', 'write': 'x', 'move': 1}, },
+#    'q2':{'0':{'next':'q3', 'write': 'x', 'move': 1}, 
+#            ' ':{'next':'qaccept', 'write': ' ', 'move': 1}, 
+#            'x':{'next':'q2', 'write': 'x', 'move': 1}, },
+#    'q3':{'0':{'next':'q4', 'write': '0', 'move': 1}, 
+#            ' ':{'next':'q5', 'write': ' ', 'move': -1}, 
+#            'x':{'next':'q3', 'write': 'x', 'move': 1}, },
+#    'q4':{'0':{'next':'q3', 'write': 'x', 'move': 1}, 
+#            ' ':{'next':'qreject', 'write': ' ', 'move': 1}, 
+#            'x':{'next':'q4', 'write': 'x', 'move': 1}, },
+#    'q5':{'0':{'next':'q5', 'write': '0', 'move': -1}, 
+#            ' ':{'next':'q2', 'write': ' ', 'move': 1}, 
+#            'x':{'next':'q5', 'write': 'x', 'move': -1}, },
+#
+#}
+machineDef['Q'] = ['q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6',]
+machineDef['Sigma'] = ['a', 'b', 'c', 'd']
+machineDef['Gamma'] = ['a', 'b', 'c', 'd', ' ', 'x']
 machineDef['Q0'] = 'q1'
-machineDef['Qaccept'] = 'qaccept'
-machineDef['Qreject'] = 'qreject'
-
+machineDef['Qaccept'] = 'q6'
+machineDef['Qreject'] = 'q0'
 machineDef['Delta'] = {
-    'q1':{'0':{'next':'q2', 'write': ' ', 'move': 1}, 
-            ' ':{'next':'qreject', 'write': ' ', 'move': 1}, 
-            'x':{'next':'qreject', 'write': 'x', 'move': 1}, },
-    'q2':{'0':{'next':'q3', 'write': 'x', 'move': 1}, 
-            ' ':{'next':'qaccept', 'write': ' ', 'move': 1}, 
-            'x':{'next':'q2', 'write': 'x', 'move': 1}, },
-    'q3':{'0':{'next':'q4', 'write': '0', 'move': 1}, 
-            ' ':{'next':'q5', 'write': ' ', 'move': -1}, 
-            'x':{'next':'q3', 'write': 'x', 'move': 1}, },
-    'q4':{'0':{'next':'q3', 'write': 'x', 'move': 1}, 
-            ' ':{'next':'qreject', 'write': ' ', 'move': 1}, 
-            'x':{'next':'q4', 'write': 'x', 'move': 1}, },
-    'q5':{'0':{'next':'q5', 'write': '0', 'move': -1}, 
-            ' ':{'next':'q2', 'write': ' ', 'move': 1}, 
-            'x':{'next':'q5', 'write': 'x', 'move': -1}, },
-
+    'q1':{'a':{'next':'q2', 'write': ' ', 'move': 1}, 
+            'b':{'next':'q0', 'write': 'b', 'move': 1},
+            'c':{'next':'q0', 'write': 'c', 'move': 1},
+            'd':{'next':'q0', 'write': 'd', 'move': 1}, 
+            ' ':{'next':'q0', 'write': ' ', 'move': 1}, 
+            'x':{'next':'q0', 'write': 'x', 'move': 1},},
+   'q2':{'b':{'next':'q3', 'write': 'x', 'move': 1}, 
+            'a':{'next':'q0', 'write': 'a', 'move': 1},
+            'c':{'next':'q0', 'write': 'c', 'move': 1},
+            'd':{'next':'q0', 'write': 'd', 'move': 1}, 
+            ' ':{'next':'q0', 'write': ' ', 'move': 1}, 
+            'x':{'next':'q0', 'write': 'x', 'move': 1},},
+   'q3':{'c':{'next':'q4', 'write': 'x', 'move': 1}, 
+            'a':{'next':'q0', 'write': 'a', 'move': 1},
+            'b':{'next':'q0', 'write': 'b', 'move': 1},
+            'd':{'next':'q0', 'write': 'd', 'move': 1}, 
+            ' ':{'next':'q0', 'write': ' ', 'move': 1}, 
+            'x':{'next':'q0', 'write': 'x', 'move': 1},},
+   'q4':{'d':{'next':'q6', 'write': 'd', 'move': 1}, 
+            'a':{'next':'q0', 'write': 'a', 'move': 1},
+            'b':{'next':'q0', 'write': 'b', 'move': 1},
+            'c':{'next':'q0', 'write': 'c', 'move': 1}, 
+            ' ':{'next':'q5', 'write': ' ', 'move': 1}, 
+            'x':{'next':'q4', 'write': 'x', 'move': -1},},
+   'q5':{'d':{'next':'q0', 'write': 'd', 'move': 1}, 
+            'a':{'next':'q0', 'write': 'a', 'move': 1},
+            'b':{'next':'q0', 'write': 'b', 'move': 1},
+            'c':{'next':'q0', 'write': 'c', 'move': 1}, 
+            ' ':{'next':'q4', 'write': ' ', 'move': -1}, 
+            'x':{'next':'q5', 'write': 'x', 'move': 1},},
 }
 
 
